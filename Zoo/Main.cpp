aa8350efa495b6bf58b6aa54c8690c5ea65b1feb
Oscar <Oscar@DIMA>
2024-10-12 15:46:15 +0300

    фиксация нового изменения

--- Zoo/Main.cpp
+++ Zoo/Main.cpp
@@ -18,6 +18,7 @@ void CaseThree(vector < Animal* > parVect, string parType);
 int main()
 {
 	//какое то добавленное изменение
+	// еще одно изменение
 	setlocale(LC_ALL, "rus");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
